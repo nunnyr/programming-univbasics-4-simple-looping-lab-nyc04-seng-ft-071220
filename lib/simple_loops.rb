c264e7f4930d5bd314bb7f4cace14df4f5a728f9
Nunny Reyes <nunnyreyes@gmail.com>
2020-07-16 17:57:11 +0000

    Automatically backed up by Learn

--- lib/simple_loops.rb
+++ lib/simple_loops.rb
@@ -1,11 +1,15 @@
 # Write your methods here
 
-def loop_message_five_times
-  
+def loop_message_five_times (string)
+  puts string
+  puts string
+  puts string
+  puts string
+  puts string
 end
 
 
-def loop_message_n_times 
+def loop_message_n_times (string)
   
 end
 
